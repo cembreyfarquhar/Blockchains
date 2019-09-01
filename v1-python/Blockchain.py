b659989f72b09ec40fb83d7a8b315cbae3a8e487
halo <cembreyfarquhar@gmail.com>
2019-09-01 13:06:51 -0500

    basic initialization near complete for blockchain class

---
+++ v1-python/Blockchain.py
@@ -0,0 +1,96 @@
+import time
+import hashlib
+import json
+
+class Blockchain:
+    def __init__(self):
+        self.pending_transactions = []
+        self.chain = []
+        self.nodes = set()
+
+        # Genesis Block
+        self.create_block("1", 100)
+
+    # @app.route("/nodes/register", methods=["POST"])
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time.time(),
+            "transactions": self.pending_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset pending transactions
+        self.pending_transactions = []
+
+        self.chain.append(block)    
+        return block
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f"{last_proof}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+
+
+    def proof_of_work(self, last_proof):
+        '''
+        Simple Proof of Work Algorithm:
+            - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+            - p is the previous proof, and p' is the new proof
+        '''
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(last_block)
+            print(block)
+            print("\n--------\n")
+            # Check that previous hash is correct
+            if block["previous_hash"] != self.hash(last_block):
+                return False
+
+            # Check that previous PoW is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+            
+            last_block = block
+            current_index += 1
+        
+        return True
+    
+    def create_transaction(self, sender, recipient, amount):
+        self.pending_transactions.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        })
+
+        return self.last_block["index"] + 1
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+
+        # Make sure the dictionairy is ordered to keep consistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    
